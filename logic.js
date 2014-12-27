36726c452bbc89c4e2e1f908f0008aeb06ccb9a0
Exodem <Exodem@gmx.de>
2014-12-27 22:39:58 +0100

    Added the parser. To use it start the node server converter.js (currently localhost).

--- logic.js
+++ logic.js
@@ -1,6 +1,19 @@
 /*Handles the data and triggers diagram updates*/
 var logic = {
-    init : function () {
+    publicationsJSON: [],
+    authors: [],
+    init: function () {
+        // create a new pubDB json object
+        var converter = new pubDB.json();
+        converter.init(function (dbObject) {
+            /*Build the JSON*/
+            converter.buildPublicationJSON(dbObject, function (pubData) {
+                logic.publications = pubData;
+                converter.buildAuthorJSON(pubData, function (authorData) {
+                    logic.authors = authorData;
+                });
+            });
+        });
 
     }
 };
--- parser.js
+++ parser.js
@@ -1,16 +1,17 @@
 /*Defines a parser Object to get the required Data from PubDB as JSON*/
 var parser = {
-    getPubDB : function (success,error) {
+    getPubDB : function () {
         $.ajax({
-            type: "POST",
-            url: "http://www.medien.ifi.lmu.de/cgi-bin/search.pl?all:all:all:all:all",
+            url: "http://www.medien.ifi.lmu.de/cgi-bin/search.pl?all:all:all:all:all:callback=?",
+            dataType: "jsonp",
             success : function (data){
-                var pubDB = parse(data);
-                success(pubDB);
-            },
-            error : function (code){
-                error(code);
+                alert("bla");
+                console.log(data);
+                return parse(data);
             }
         });
+    },
+    parse : function (data){
+        return data;
     }
 };
